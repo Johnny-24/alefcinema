203b3500edd4762e8d54b284ed4705d7bb392e63
Johnny <dos_24@inbox.ru>
2023-11-29 16:55:47 +0500

    add main page html

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='home'),
+	path('catalog', views.catalog, name='catalog'),
+  path('delivery', views.delivery, name='delivery'),
+  path('about', views.about, name='about'),
+  path('contacts', views.contacts, name='contacts'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 
+menu = [
+  { "id": 1, "label": "Каталог", "url": "catalog" },
+  { "id": 2, "label": "Доставка", "url": "delivery" },
+  { "id": 3, "label": "О компании", "url": "about" },
+  { "id": 4, "label": "Контакты", "url": "contacts" }
+]
+
 def index(request):
-  return render(request, 'mainapp/index.html')
+  return render(request, 'mainapp/index.html', { "menu": menu })
+
+def catalog(request):
+  return render(request, 'mainapp/catalog.html', { "menu": menu })
+
+def delivery(request):
+  return render(request, 'mainapp/delivery.html', { "menu": menu })
+
+def about(request):
+  return render(request, 'mainapp/about.html', { "menu": menu })
+
+def contacts(request):
+  return render(request, 'mainapp/contacts.html', { "menu": menu })
